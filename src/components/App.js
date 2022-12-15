a36853fd22688eb26f686042ee2e5f56c851488f
Daniel <dhonaruto@gmail.com>
2022-12-15 14:26:12 -0500

    Implement Authentication for User #23

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext'
 
 import AboutUs from './AboutUs'
 import SignUp from './signup/SignUp'
@@ -8,13 +9,16 @@ import Login from './login/Login'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<AboutUs/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<AboutUs/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
+    
   )
 }
 
---
+++ src/components/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUXkceWD1BGSnJsKWZVZRNVfKrnT2608w",
+  authDomain: "boba-love-5e8a4.firebaseapp.com",
+  projectId: "boba-love-5e8a4",
+  storageBucket: "boba-love-5e8a4.appspot.com",
+  messagingSenderId: "953248647818",
+  appId: "1:953248647818:web:f283cc822f2092b4607b73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export default app;
